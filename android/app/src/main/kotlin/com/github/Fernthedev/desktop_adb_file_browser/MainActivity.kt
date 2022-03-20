6b96afbdcae4cb672ef54a2d6afdf9e789495ab0
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-03-20 10:39:00 -0400

    Initial setup with cool theme

---
+++ android/app/src/main/kotlin/com/github/Fernthedev/desktop_adb_file_browser/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.Fernthedev.desktop_adb_file_browser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,218 @@
+import 'package:desktop_adb_file_browser/pages/devices.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:routemaster/routemaster.dart';
+
+final routes = RouteMap(routes: {
+  '/': (_) => const MaterialPage(
+          child: MyHomePage(
+        title: 'hi!',
+      )),
+
+  '/devices': (_) => const MaterialPage(child: DevicesPage())
+
+  // '/feed': (_) => MaterialPage(child: FeedPage()),
+  // '/settings': (_) => MaterialPage(child: SettingsPage()),
+  // '/feed/profile/:id': (info) => MaterialPage(
+  //     child: ProfilePage(id: info.pathParameters['id'])
+  // ),
+});
+
+void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerDelegate: RoutemasterDelegate(routesBuilder: (context) => routes),
+      routeInformationParser: const RoutemasterParser(),
+
+      title: 'ADB File Manager',
+      // https://rydmike.com/flexcolorschemeV4Tut5/#/
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.sanJuanBlue,
+        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
+        blendLevel: 18,
+        appBarStyle: FlexAppBarStyle.primary,
+        appBarOpacity: 0.95,
+        appBarElevation: 0,
+        transparentStatusBar: true,
+        tabBarStyle: FlexTabBarStyle.forAppBar,
+        tooltipsMatchBackground: true,
+        swapColors: false,
+        lightIsWhite: false,
+        useSubThemes: true,
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        // To use playground font, add GoogleFonts package and uncomment:
+        fontFamily: GoogleFonts.notoSans().fontFamily,
+        subThemesData: const FlexSubThemesData(
+          useTextTheme: true,
+          fabUseShape: true,
+          interactionEffects: true,
+          bottomNavigationBarElevation: 0,
+          bottomNavigationBarOpacity: 0.95,
+          navigationBarOpacity: 0.95,
+          navigationBarMutedUnselectedText: true,
+          navigationBarMutedUnselectedIcon: true,
+          inputDecoratorIsFilled: true,
+          inputDecoratorBorderType: FlexInputBorderType.outline,
+          inputDecoratorUnfocusedHasBorder: true,
+          blendOnColors: true,
+          blendTextTheme: true,
+          popupMenuOpacity: 0.95,
+        ),
+      ),
+      darkTheme: FlexThemeData.dark(
+        scheme: FlexScheme.sanJuanBlue,
+        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
+        blendLevel: 18,
+        appBarStyle: FlexAppBarStyle.background,
+        appBarOpacity: 0.95,
+        appBarElevation: 0,
+        transparentStatusBar: true,
+        tabBarStyle: FlexTabBarStyle.forAppBar,
+        tooltipsMatchBackground: true,
+        swapColors: false,
+        darkIsTrueBlack: false,
+        useSubThemes: true,
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        // To use playground font, add GoogleFonts package and uncomment:
+        fontFamily: GoogleFonts.notoSans().fontFamily,
+        subThemesData: const FlexSubThemesData(
+          useTextTheme: true,
+          fabUseShape: true,
+          interactionEffects: true,
+          bottomNavigationBarElevation: 0,
+          bottomNavigationBarOpacity: 0.95,
+          navigationBarOpacity: 0.95,
+          navigationBarMutedUnselectedText: true,
+          navigationBarMutedUnselectedIcon: true,
+          inputDecoratorIsFilled: true,
+          inputDecoratorBorderType: FlexInputBorderType.outline,
+          inputDecoratorUnfocusedHasBorder: true,
+          blendOnColors: true,
+          blendTextTheme: true,
+          popupMenuOpacity: 0.95,
+        ),
+      ),
+      // If you do not have a themeMode switch, uncomment this line
+      // to let the device system mode control the theme mode:
+      // themeMode: ThemeMode.system,
+      themeMode: ThemeMode.dark,
+      // theme: ThemeData(
+      //   // This is the theme of your application.
+      //   //
+      //   // Try running your application with "flutter run". You'll see the
+      //   // application has a blue toolbar. Then, without quitting the app, try
+      //   // changing the primarySwatch below to Colors.green and then invoke
+      //   // "hot reload" (press "r" in the console where you ran "flutter run",
+      //   // or simply save your changes to "hot reload" in a Flutter IDE).
+      //   // Notice that the counter didn't reset back to zero; the application
+      //   // is not restarted.
+      //   primarySwatch: Colors.blue,
+      // ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            ElevatedButton(
+                onPressed: () => Routemaster.of(context).push('/devices'),
+                child: const Icon(Icons.devices))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}

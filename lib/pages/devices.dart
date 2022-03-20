298c8b7133a12fcc16ae4d7296a4375679c5ff6a
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-03-20 11:59:57 -0400

    cool device card

--- lib/pages/devices.dart
+++ lib/pages/devices.dart
@@ -1,3 +1,4 @@
+import 'package:desktop_adb_file_browser/widgets/device_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -21,27 +22,18 @@ class DevicesPage extends StatelessWidget {
         title: const Text(title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[],
-        ),
-      ),
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: GridView.extent(
+              childAspectRatio: 16.0 / 8.0,
+              padding: const EdgeInsets.all(4.0),
+              mainAxisSpacing: 4.0,
+              crossAxisSpacing: 4.0,
+              maxCrossAxisExtent: 250,
+              children: const [
+            DeviceCard("Android Phone", "Google", "WBN23B1I3N1KNNEWJ"),
+            DeviceCard("Android Phone", "Google", "WBN23B1I3N1KNNEWJ")
+          ])),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {},
         tooltip: 'Refresh',
---
+++ lib/widgets/device_card.dart
@@ -0,0 +1,47 @@
+import 'dart:ui';
+
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:flutter/material.dart';
+
+@immutable
+class DeviceCard extends StatelessWidget {
+  const DeviceCard(this.deviceName, this.deviceManufacturer, this.serialName,
+      {Key? key})
+      : super(key: key);
+
+  final String deviceName;
+  final String deviceManufacturer;
+  final String serialName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        onTap: () => {},
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Expanded(
+              child: ListTile(
+                leading: const Icon(
+                  FluentIcons.phone_24_regular,
+                  size: 24 * 1.6,
+                ),
+                title: Text(deviceName),
+                subtitle: Text(deviceManufacturer),
+              ),
+              flex: 2,
+            ),
+            Expanded(
+              child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child:
+                      TextButton(onPressed: () => {}, child: Text(serialName))),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

46ede5831b87792ed87bf4ab0c5299978837271d
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-08-12 00:08:24 -0400

    move

--- lib/pages/ADBCheck.dart
+++ lib/pages/ADBCheck.dart
@@ -1,5 +1,6 @@
 import 'package:desktop_adb_file_browser/main.dart';
 import 'package:desktop_adb_file_browser/utils/adb.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:routemaster/routemaster.dart';
 
@@ -48,6 +49,54 @@ class _ADBCheckState extends State<ADBCheck> {
     return Scaffold(
       appBar:
           AppBar(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ADBDownloadDialog extends StatefulWidget {
+  const ADBDownloadDialog({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ADBDownloadDialog> createState() => _ADBDownloadDialogState();
+}
+
+class _ADBDownloadDialogState extends State<ADBDownloadDialog> {
+  int? current;
+  int? total;
+  CancelToken cancelToken = CancelToken();
+
+  @override
+  Widget build(BuildContext context) {
+    if (current == null || total == null) return _initialDownloadDialog();
+
+    return AlertDialog(
+        title: const Text("Downloading ADB"),
+        content: LinearProgressIndicator(
+          value: current!.toDouble() / total!.toDouble(),
+        ));
+  }
+
+  Widget _initialDownloadDialog() {
+    return AlertDialog(
+      title: const Text("ADB not found"),
+      content: const Text("Do you want to download ABD?"),
+      actions: [
+        TextButton(
+            onPressed: () async {
+              await Adb.downloadADB((c, t) {
+                setState(() {
+                  current = c;
+                  total = t;
+                });
+              }, cancelToken);
+
+              if (!mounted) return;
+              Navigator.pop(context);
+            },
+            child: const Text("Ok"))
+      ],
     );
   }
 }

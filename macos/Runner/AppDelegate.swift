16004ac4dc01a31c72dda47537d2273e015a61f3
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-05-13 17:02:10 -0400

    Update Flutter runtime build scripts

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,10 +3,6 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  override func applicationDidBecomeActive(_ notification: Notification) {
-      signal(SIGPIPE, SIG_IGN) //Ignore signal
-  }
-    
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
